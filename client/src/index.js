7bedb310404df497b7b88896cf1f3a76499ffb0f
Jeremy Lehman <jeremylehman9@gmail.com>
2016-07-30 14:00:13 -0400

    modified folder structure, added routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import routes from './conf/routes';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  routes,
   document.getElementById('root')
 );
